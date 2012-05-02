293f04fef8bafb19e5c7baf164f2c56ca3ea02a9
Sean Durham <sean@Seans-Home-Mac-2.local>
2012-05-02 16:28:34 -0700

    Enable configuring different behavior on each element dragon is applied to.

--- spec/javascripts/dragon_spec.js
+++ spec/javascripts/dragon_spec.js
@@ -1,15 +1,35 @@
 describe("Dragon.js", function(){
   beforeEach(function(){
     setFixtures('<div class="square"></div><div class="circle"></div>');
-    console.dir($(".square"))
     this.dragon_el = $(".square").dragon();
   });
-  
+
   it("adds dragon method to jQuery", function(){
     expect(typeof $().dragon).toBe("function");
   });
-  
+
   it("activates dragon on an element", function(){
     expect(this.dragon_el).toBeDefined();
+  });
+
+  describe("with multiple elements", function(){
+    beforeEach(function(){
+      this.square_el = $('.square').dragon({
+        horizontal:true
+      });
+      this.circle_el = $('.circle').dragon({
+        vertical:true
+      });
+      this.square_id = this.square_el.data('dragon_id');
+      this.circle_id = this.circle_el.data('dragon_id');
+    });
+
+    it("can configure multiple elements", function(){
+      console.log(this.square_el.dragon.settings);
+      expect(this.square_el.dragon.settings[this.square_id].horizontal).toBe(true);
+      expect(this.square_el.dragon.settings[this.square_id].vertical).toBe(false);
+      expect(this.circle_el.dragon.settings[this.circle_id].horizontal).toBe(false);
+      expect(this.circle_el.dragon.settings[this.circle_id].vertical).toBe(true);
+    });
   });
 });
--- src/dragon.js
+++ src/dragon.js
@@ -6,17 +6,18 @@
 
 (function($) {
 
+    var id = 0;
     $.fn.dragon = function(method) {
 
         var drag = {};
-
         // Determine if device is a touch device
         var isTouchDevice = function() {
           var el = document.createElement('div');
+
           el.setAttribute('ongesturestart', 'return;');
           return (typeof el.ongesturestart == "function") ? true : false;
         }
-        
+
         if (isTouchDevice()){
           // prevent scrolling the screen with touch dragging
           $('html').bind('touchmove', function(){ event.preventDefault() });
@@ -30,11 +31,12 @@
         // Find the position of the mouse or of a touch
         var inputX = function(e){return isTouchDevice() ? e.originalEvent.touches[0].clientX : e.pageX;}
         var inputY = function(e){return isTouchDevice() ? e.originalEvent.touches[0].clientY : e.pageY;}
-        
+
         // Parse translate3D CSS values
         var matrixToArray = function(matrix) {
           return matrix.substr(7, matrix.length - 8).split(', ');
         }
+
 
         // public methods
         // to keep the $.fn namespace uncluttered, collect all of the plugin's methods in an object literal and call
@@ -54,21 +56,22 @@
             // this the constructor method that gets called when the object is created
             init : function(options) {
 
-                // the plugin's final properties are the merged default and user-provided properties (if any)
-                // this has the advantage of not polluting the defaults, making them re-usable
-                this.dragon.settings = $.extend({}, this.dragon.defaults, options);
-
+                var dragon = this.dragon
                 // iterate through all the DOM elements we are attaching the plugin to
-                return this.each(function() {
+                var els = this.each(function() {
 
                     var $element = $(this), // reference to the jQuery version of the current DOM element
                         element = this;     // reference to the actual DOM element
-                    
-                    // code goes here
-                    $element.bind(dragStartEventType, helpers.onDragStart);                    
-                    
-                });
+                    $element.data('dragon_id', id);
 
+                    // the plugin's final properties are the merged default and user-provided properties (if any)
+                    // this has the advantage of not polluting the defaults, making them re-usable
+                    dragon.settings[id] = $.extend({}, dragon.defaults, options);
+
+                    $element.bind(dragStartEventType, helpers.onDragStart);
+                    id++;
+                });
+                return els;
             }
 
         }
@@ -84,13 +87,14 @@
 
           onDragStart: function(event){
             var $element = $(this);
-          
-            $target = $.fn.dragon.settings.target;
-            
+            var id = $element.data('dragon_id');
+
+            var $target = $.fn.dragon.settings[id].target;
+
             if (typeof $target === "undefined"){
               $target = $element;
             }
-                    
+
             drag = {
               startInputX: inputX(event),
               startInputY: inputY(event),
@@ -98,161 +102,161 @@
               lastInputY: inputY(event),
               direction: null
             }
-            $(document).bind(dragMoveEventType, helpers.onDragMove);
-            $(document).bind(dragEndEventType, helpers.onDragEnd);
-          },
-          
-          onDragMove: function(event){
-            mouseDiff = helpers.getMouseDiff(event);
-            newPosition = helpers.getNewTargetPosition(mouseDiff);
-            helpers.translate(newPosition);
-          },
-          
-          onDragEnd: function(){  
-            $(document).unbind(dragMoveEventType, helpers.onDragMove);
-            $(document).unbind(dragEndEventType, helpers.onDragEnd);
-            
-            var matrix = matrixToArray($target.css("-webkit-transform")); 
-            $target.endX = parseInt(matrix[4]);
-            $target.endY = parseInt(matrix[5]);
-            
-            helpers.log(matrix);
-            
-            var deltaX = Math.abs($target.startX - $target.endX);
-            var deltaY = Math.abs($target.startY - $target.endY);
+            $(document).bind(dragMoveEventType, onDragMove);
+            $(document).bind(dragEndEventType, onDragEnd);
 
-            helpers.log("x=" + deltaX + " " + "y=" + deltaY);
-            
-            if (typeof $.fn.dragon.settings[drag.direction].end !== "undefined"){
-              switch(drag.direction) 
-              {
-              case "up":
-              case "down":
-                var delta = deltaY;
-                break;
-              case "left":
-              case "right":
-                var delta = deltaX;
-                break;
-              } 
-                            
-              if (delta > $.fn.dragon.settings[drag.direction].threshold){
-                $.fn.dragon.settings[drag.direction].end();
-              }
-
-            }
-          },
-            
-          getMouseDiff: function(event){
-            var diffX = inputX(event) - drag.lastInputX;
-            var diffY = inputY(event) - drag.lastInputY;
-            
-            drag.lastInputX = inputX(event);
-            drag.lastInputY = inputY(event);
-            
-            return {
-              x: diffX,
-              y: diffY
-            }
-          },
-          
-          getNewTargetPosition: function(mouseDiff){
-            var newX = mouseDiff.x;
-            var newY = mouseDiff.y;
-          
-            if (drag.direction == null) {
-              drag.direction = helpers.getDirection(drag.startInputX, drag.startInputY, drag.lastInputX, drag.lastInputY);
-                            
-              if (drag.direction != null){
-                  helpers.log(drag.direction);
-                  helpers.log("t=" + $.fn.dragon.settings[drag.direction].target);
-                  helpers.log($target);
-                  if (typeof $.fn.dragon.settings[drag.direction].target !== "undefined") {
-                    helpers.log($.fn.dragon.settings[drag.direction].target);
-                    $target = $.fn.dragon.settings[drag.direction].target;
-                  }
-              }
-
-              if($target.css('-webkit-transform') == 'none'){
-                $target.css('-webkit-transform', 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)');
-              }
-              
-              var matrix = matrixToArray($target.css("-webkit-transform")); 
-              $target.startX = parseInt(matrix[4]);
-              $target.startY = parseInt(matrix[5]);
-            
-              return {x: 0, y: 0}
+            function onDragMove(event){
+              mouseDiff = getMouseDiff(event);
+              newPosition = getNewTargetPosition(mouseDiff, id);
+              translate(newPosition);
             }
 
-            if($.fn.dragon.settings.horizontal === true && $.fn.dragon.settings.vertical === false){
-              newY = 0;
-            } else if($.fn.dragon.settings.horizontal === false && $.fn.dragon.settings.vertical === true){
-              newX = 0;
-            } else if ($.fn.dragon.settings.horizontal === true && $.fn.dragon.settings.vertical === true) {
-              switch(drag.direction) 
-              {
-              case "up":
-              case "down":
-                newX = 0;
-                break;
-              case "left":
-              case "right":
+            function onDragEnd(){
+              $(document).unbind(dragMoveEventType, onDragMove);
+              $(document).unbind(dragEndEventType, onDragEnd);
+
+              var matrix = matrixToArray($target.css("-webkit-transform"));
+              $target.endX = parseInt(matrix[4]);
+              $target.endY = parseInt(matrix[5]);
+
+              helpers.log(matrix);
+
+              var deltaX = Math.abs($target.startX - $target.endX);
+              var deltaY = Math.abs($target.startY - $target.endY);
+
+              helpers.log("x=" + deltaX + " " + "y=" + deltaY);
+
+              if (typeof $.fn.dragon.settings[id][drag.direction].end !== "undefined"){
+                switch(drag.direction)
+                {
+                case "up":
+                case "down":
+                  var delta = deltaY;
+                  break;
+                case "left":
+                case "right":
+                  var delta = deltaX;
+                  break;
+                }
+
+                if (delta > $.fn.dragon.settings[id][drag.direction].threshold){
+                  $.fn.dragon.settings[id][drag.direction].end();
+                }
+
+              }
+            }
+
+            function getMouseDiff(event){
+              var diffX = inputX(event) - drag.lastInputX;
+              var diffY = inputY(event) - drag.lastInputY;
+
+              drag.lastInputX = inputX(event);
+              drag.lastInputY = inputY(event);
+
+              return {
+                x: diffX,
+                y: diffY
+              }
+            }
+
+            function getNewTargetPosition(mouseDiff){
+              var newX = mouseDiff.x;
+              var newY = mouseDiff.y;
+
+              if (drag.direction == null) {
+                drag.direction = getDirection(drag.startInputX, drag.startInputY, drag.lastInputX, drag.lastInputY);
+
+                if (drag.direction != null){
+                    //helpers.log(drag.direction);
+                    //helpers.log("t=" + $.fn.dragon.settings[id][drag.direction].target);
+                    //helpers.log($target);
+                    if (typeof $.fn.dragon.settings[id][drag.direction].target !== "undefined") {
+                      helpers.log($.fn.dragon.settings[id][drag.direction].target);
+                      $target = $.fn.dragon.settings[id][drag.direction].target;
+                    }
+                }
+
+                if($target.css('-webkit-transform') == 'none'){
+                  $target.css('-webkit-transform', 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)');
+                }
+
+                var matrix = matrixToArray($target.css("-webkit-transform"));
+                $target.startX = parseInt(matrix[4]);
+                $target.startY = parseInt(matrix[5]);
+
+                return {x: 0, y: 0}
+              }
+
+              if($.fn.dragon.settings[id].horizontal === true && $.fn.dragon.settings[id].vertical === false){
                 newY = 0;
-                break;
-              }                  
+              } else if($.fn.dragon.settings[id].horizontal === false && $.fn.dragon.settings[id].vertical === true){
+                newX = 0;
+              } else if ($.fn.dragon.settings[id].horizontal === true && $.fn.dragon.settings[id].vertical === true) {
+                switch(drag.direction)
+                {
+                case "up":
+                case "down":
+                  newX = 0;
+                  break;
+                case "left":
+                case "right":
+                  newY = 0;
+                  break;
+                }
+              }
+
+              return {
+                x: newX,
+                y: newY
+              }
             }
-            
-            return {
-              x: newX,
-              y: newY
+
+            function translate(position){
+              var matrix = matrixToArray($target.css("-webkit-transform"));
+              var newX = parseInt(matrix[4]) + position.x;
+              var newY = parseInt(matrix[5]) + position.y;
+
+              $target.css({
+                translateX:newX,
+                translateY:newY
+              });
+
+              //$target.css('-webkit-transform', 'translate3d(' + newX + 'px,'+ newY +'px,0)');
+            }
+
+            function move(position){
+              $target.css('-webkit-transition','all 100ms ease-in-out')
+                     .css('-webkit-transform', 'translate3d(0px,400px,0)');
+            }
+
+            function getDirection(startX, startY, lastX, lastY){
+              var diffX = lastX - startX;
+              var diffY = lastY - startY;
+              if(diffX > 2 || diffX < -2 || diffY > 2 || diffY < -2){
+                var angle = getAngle(startX, startY, lastX,lastY);
+                if (angle <= -30 && angle >= -150){
+                  return 'up';
+                }
+                else if (angle < 150 && angle > 30){
+                  return 'down';
+                }
+                else if (angle <= 30 && angle >= -30 ){
+                  return 'right';
+                }
+                else {
+                  return 'left';
+                }
+              };
+              return null;
+            }
+
+            function getAngle(startX, startY, lastX,lastY) {
+              var angle = Math.atan2(lastY - startY, lastX - startX);
+              var degree = angle * 180/Math.PI;
+              return degree;
             }
           },
-          
-          translate: function(position){
-            var matrix = matrixToArray($target.css("-webkit-transform"));
-            var newX = parseInt(matrix[4]) + position.x;
-            var newY = parseInt(matrix[5]) + position.y;
-            
-            $target.css({
-              translateX:newX,
-              translateY:newY
-            });
 
-            //$target.css('-webkit-transform', 'translate3d(' + newX + 'px,'+ newY +'px,0)');
-          },
-
-          move: function(position){
-            $target.css('-webkit-transition','all 100ms ease-in-out')
-                   .css('-webkit-transform', 'translate3d(0px,400px,0)');
-          },
-
-          getDirection: function(startX, startY, lastX, lastY){
-            var diffX = lastX - startX;
-            var diffY = lastY - startY;
-            if(diffX > 2 || diffX < -2 || diffY > 2 || diffY < -2){
-              var angle = helpers.getAngle(startX, startY, lastX,lastY);
-              if (angle <= -30 && angle >= -150){
-                return 'up';
-              }
-              else if (angle < 150 && angle > 30){
-                return 'down';
-              }
-              else if (angle <= 30 && angle >= -30 ){
-                return 'right';
-              }
-              else {
-                return 'left';
-              }
-            };
-            return null;
-          },
-          
-          getAngle: function(startX, startY, lastX,lastY) {
-            var angle = Math.atan2(lastY - startY, lastX - startX);
-            var degree = angle * 180/Math.PI;    
-            return degree;
-          },
-          
           log: function(message){
             LOG_ENABLED = false;
             if (LOG_ENABLED){
@@ -289,10 +293,10 @@
         target: undefined,
         horizontal: false,
         vertical: false,
-        right: undefined,
-        left: undefined,
-        up: undefined,
-        down: undefined,
+        right: {},
+        left: {},
+        up: {},
+        down: {},
     }
 
     // this will hold the merged default and user-provided options
@@ -300,6 +304,6 @@
     // this.dragon.settings.propertyName from inside the plugin or
     // element.dragon.settings.propertyName from outside the plugin, where "element" is the element the
     // plugin is attached to;
-    $.fn.dragon.settings = {}
+    $.fn.dragon.settings = {};
 
 })(jQuery);
