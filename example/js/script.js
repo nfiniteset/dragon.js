491a4971e871606b8603acfdb88eba2ad413d39d
Sean Durham <sean@Seans-Home-Mac-2.local>
2012-05-02 17:08:30 -0700

    Simplify example.

--- example/js/script.js
+++ example/js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-  $('.box').dragon({
+  $('.square').dragon({
     horizontal: true,
     vertical: true,
     right: {
-      target: $('.box'),
       threshold: 200,
       end: function(){
         $('.box').animate({
@@ -13,31 +12,14 @@ $(document).ready(function(){
             rotateY: '+='+(2*Math.PI),
             rotateX: '+='+Math.PI,
             rotateZ: '+='+Math.PI
-        },100);    
-      }
-    },
-    left: {
-      target: $('.box'),
-      threshold: 200,
-      endposition: -400,
-      speed: '150ms',
-      end: function(){
-      }
-    },
-    up: {
-      threshold: 200,
-      endposition: -400,
-      speed: '150ms',
-      end: function(){
+        },100);
       }
     },
     down: {
-      target: $('.circle'),
-      threshold: 200,
-      endposition: -400,
-      speed: '150ms',
-      end: function(){
-      }
+      target: $('.circle')
     }
+  });
+  $('.circle').dragon({
+    horizontal: true
   });
 });
